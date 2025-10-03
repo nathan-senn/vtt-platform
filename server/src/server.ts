2f0196ee55367eebcc02f4a347ec21d10c2f4efd
Martin Elsaesser <marels@aon.at>
2025-10-03 15:02:10 +0200

    add socket.io server

---
+++ server/src/server.ts
@@ -0,0 +1,28 @@
+import { Server } from "socket.io";
+
+export interface ServerToClientEvents {
+  getMessagesFromServer: (data: { messages: string[] }) => void;
+}
+
+export interface ClientToServerEvents {
+  newMessageFromClient: (data: { message: string }) => void;
+}
+
+const chatMessages: string[] = [];
+
+const io = new Server<ClientToServerEvents, ServerToClientEvents>({
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.emit("getMessagesFromServer", { messages: chatMessages });
+
+  socket.on("newMessageFromClient", ({ message }) => {
+    chatMessages.push(message);
+    io.emit("getMessagesFromServer", { messages: chatMessages });
+  });
+});
+
+io.listen(3000);
