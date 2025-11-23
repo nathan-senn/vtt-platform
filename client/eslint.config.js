a2aee84ab6a22d2e702e0fa8139a0004ff8deee7
Martin Elsaesser <marels@aon.at>
2025-11-23 17:27:31 +0100

    configure eslint for typed linting

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -8,16 +8,47 @@ import { defineConfig, globalIgnores } from "eslint/config";
 export default defineConfig([
 	globalIgnores(["dist"]),
 	{
-		files: ["**/*.{ts,tsx}"],
+		files: ["**/*.{mts,ts,tsx}"],
 		extends: [
 			js.configs.recommended,
-			tseslint.configs.recommended,
+			tseslint.configs.recommendedTypeChecked,
 			reactHooks.configs["recommended-latest"],
 			reactRefresh.configs.vite,
 		],
 		languageOptions: {
 			ecmaVersion: 2020,
 			globals: globals.browser,
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: import.meta.dirname,
+			},
 		},
+	},
+	{
+		rules: {
+			"@typescript-eslint/no-unused-vars": [
+				"warn",
+				{
+					args: "all",
+					argsIgnorePattern: "^_",
+					caughtErrors: "all",
+					caughtErrorsIgnorePattern: "^_",
+					destructuredArrayIgnorePattern: "^_",
+					varsIgnorePattern: "^_",
+					ignoreRestSiblings: true,
+				},
+			],
+			"prefer-template": "warn",
+			"object-shorthand": "warn",
+		},
+	},
+	{
+		files: ["**/*.{mjs,js,jsx}"],
+		extends: [
+			js.configs.recommended,
+			tseslint.configs.recommended,
+			reactHooks.configs["recommended-latest"],
+			reactRefresh.configs.vite,
+		],
 	},
 ]);
